5571fa5c501bf7bbf12e20c4f5a81dd5a22d9746
Ankita Singh <ankitasingh@Ankitas-MacBook-Pro.local>
2023-12-07 11:13:58 +0530

    impelementation of mocha reports

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,18 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',  // for generating html report
+  reporterOptions: {
+    charts: true,
+    reportPageTitle: 'custom-title',
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false,
+  },
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      this.screenshotOnRunFailure=true;
+      require('cypress-mochawesome-reporter/plugin')(on);  // for html report
     },
   },
 });
